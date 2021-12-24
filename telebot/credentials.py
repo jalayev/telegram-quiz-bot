5697323c37fa83e2e93e47b5c641a03751fdd164
Teimuraz-Jalayev <jalayev01@gmail.com>
2021-12-24 22:25:28 +0600

    fixed URL link to the deployed app

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
 bot_token = "5095985566:AAHUDs4B8GCrZulUhPyStlW2R5hMiZ4Z3ck"
 bot_user_name = "quiz_testgame_bot"
-URL = "https://git.heroku.com/quiz-testgame.git"
+URL = "https://quiz-testgame.herokuapp.com/"
