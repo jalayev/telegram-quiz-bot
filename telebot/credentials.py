5ac75be7433aeaa2ba998c59cfa0e254ddd0d216
Teimuraz-Jalayev <jalayev01@gmail.com>
2021-12-27 22:10:12 +0600

    fixed URL link

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,4 +1,4 @@
 bot_token = "5095985566:AAHUDs4B8GCrZulUhPyStlW2R5hMiZ4Z3ck"
 bot_user_name = "quiz_testgame_bot"
-URL = "https://edf4-2-133-5-198.ngrok.io/"
-URL2 = "https://quiz-testgame.herokuapp.com/"
+URL = "https://quiz-testgame.herokuapp.com/"
+# URL2 = "https://quiz-testgame.herokuapp.com/"
